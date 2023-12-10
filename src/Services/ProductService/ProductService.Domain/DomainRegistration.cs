5c818472d625ccdbacf6a545a583c92e98241996
Özcan Çaparoğlu <ozcan.caparoglu@gmail.com>
2023-12-10 21:08:45 +0300

    Fucking nuget package problems

---
+++ src/Services/ProductService/ProductService.Domain/DomainRegistration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ProductService.Domain.Persistence;
+using UsedCables.Infrastructure.Cache.Redis;
+using UsedCables.Infrastructure.Configuration;
+
+namespace ProductService.Domain
+{
+    public static class DomainRegistration
+    {
+        public static IServiceCollection AddDomainServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            //For Dapper
+            services.Configure<ConnectionStrings>(configuration.GetSection("ConnectionStrings"));
+
+            //For EF Core
+            services.AddDbContext<AppDbContext>(options => options.UseNpgsql(configuration.GetConnectionString("AppConnectionString")));
+
+            services.Configure<RedisConfigurationOptions>(configuration.GetSection("RedisDatabase"));
+
+            services.AddScoped<DbContext, AppDbContext>();
+
+            return services;
+        }
+    }
+}
